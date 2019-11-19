84cb6616b27dd19fe6b1afe931f8cbc27e7c71ca
courage173 <courageosemwengie@gmail.com>
2019-11-19 10:50:07 +0100

    Post,Update,Get,Delete endpoints for category.

--- backend/models/migration.js
+++ backend/models/migration.js
@@ -69,9 +69,17 @@ CREATE TABLE gif (
         REFERENCES users (id)
         );
     
+    DROP TABLE IF EXISTS categories CASCADE;
+    CREATE TABLE categories (
+        category_id INTEGER NOT NULL,
+        category_name varchar NOT NULL,        
+        PRIMARY KEY (category_id),
+        FOREIGN KEY (user_id) 
+        );
     
 
 
 `);
+//load
 
 export default migrate;
