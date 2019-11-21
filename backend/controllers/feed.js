f0aaad53baeb70ddc8a75a0184fd0c3184fa4764
courage173 <courageosemwengie@gmail.com>
2019-11-22 00:11:16 +0100

    migration database for travis updated

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -3,6 +3,12 @@ import FeedService from '../models/fieldDb'
 
 const getFeed = async (req,res)=>{
     const result = await FeedService.getFeed()
+    if(!result){
+        res.status(200).json({
+            error: "error",
+            message: "No Aricles or gifs Post found"
+        })
+    }
     res.status(200).json({
         status: "success",
         data: result
