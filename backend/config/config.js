fac5eb972383f780f45ebfb16cb89a00c3cb4aba
courage173 <courageosemwengie@gmail.com>
2019-11-21 04:28:33 +0100

    set up dotenv configuration

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,18 +1,36 @@
-import dotenv from 'dotenv';
 
+import dotenv from 'dotenv'
+import { Pool } from 'pg'
 dotenv.config();
 
-const config = {
-  port: process.env.PORT || 3000,
-  env: process.env.NODE_ENV,
-  
-  databaseUrl: {
-    development: process.env.development_URL,
-    test: process.env.test_URL,
-    production: process.env.DB_URL
- }
-};
+console.log(`This is a ${process.env.NODE_ENV} environment`);
 
+let pool;
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const testConnectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/test_teamwork`;
 
-export default config;
+if (process.env.NODE_ENV === 'production') {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === 'production'
+  });
+} else if (process.env.NODE_ENV === 'test') {
+  pool = new Pool({
+    connectionString: ConnectionString
+  });
+} else {
+  pool = new Pool({
+    connectionString
+  });
+}
 
+if (!pool) {
+  console.log('Database Setup  Was Unsuccessful');
+  process.exit(1);
+} else {
+  pool.on('connect', () => {
+    console.log('connected to the Database Successfully');
+  });
+}
+
+export default pool;
